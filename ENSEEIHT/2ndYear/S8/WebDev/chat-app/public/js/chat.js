4f8d6046f2af12eabe8c8a0af6def57346e07313
ysaoudi <younes.saoudi@outlook.fr>
2021-03-03 22:53:26 +0100

    added rooms, auto scroll, corrected message replication and styling

--- ENSEEIHT/2ndYear/S8/WebDev/chat-app/public/js/chat.js
+++ ENSEEIHT/2ndYear/S8/WebDev/chat-app/public/js/chat.js
@@ -1,11 +1,104 @@
 const socket = io()
 
-socket.on("message", (msg) => {
-    console.log("Received message:", msg)
+// Elements
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = $messageForm.querySelector("input")
+const $messageFormButton = $messageForm.querySelector("button")
+const $sendLocationButton = document.querySelector("#send-location")
+const $messages = document.querySelector("#messages")
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML
+
+// Options
+const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
+// Auto Scroll
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+socket.on("locationMessage", ({username, url, createdAt}) => {
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate, {username, url, createdAt: moment(createdAt).format("hh:mm A")})
+    $messages.insertAdjacentHTML("beforeend", html)
+    autoscroll()
 })
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+socket.on("message", ({username, text, createdAt}) => {
+    console.log("Received message:", text)
+    const html = Mustache.render(messageTemplate, {username, message: text, createdAt: moment(createdAt).format("hh:mm A")})
+    $messages.insertAdjacentHTML("beforeend", html)
+    autoscroll()
+})
+
+
+socket.on("roomData", ({room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {room, users})
+    document.querySelector("#sidebar").innerHTML = html
+})
+
+$messageForm.addEventListener("submit", (e) => {
     e.preventDefault()
+    $messageFormButton.setAttribute("disabled", "disabled")
+
     const message = e.target.elements.message.value
-    socket.emit("sendMessage", message)
+
+    socket.emit("sendMessage", message, (error) => {
+        $messageFormButton.removeAttribute("disabled")
+        $messageFormInput.value = ""
+        $messageFormInput.focus()
+
+        if(error)
+            return console.log(error)
+        
+        console.log("Message delivered!")
+    })
+})
+
+$sendLocationButton.addEventListener("click", () => {
+    if(!navigator.geolocation)
+        return alert("Geolocation not supported by your browser.")
+    
+    $sendLocationButton.setAttribute("disabled", "disabled")
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit("sendLocation", {
+            latitude: position.coords.latitude,
+            longitude:position.coords.longitude
+        }, (error) => {
+            $sendLocationButton.removeAttribute("disabled")
+
+            if(error)
+                return console.log(error)
+            console.log("Location shared!")
+        })
+    }) 
+})
+
+socket.emit("join", {username, room}, (error) => {
+    if(error){
+        alert(error)
+        location.href = "/"
+    }    
 })
--- ENSEEIHT/2ndYear/S8/WebDev/chat-app/src/index.js
+++ ENSEEIHT/2ndYear/S8/WebDev/chat-app/src/index.js
@@ -2,6 +2,14 @@ const express = require("express")
 const path = require("path")
 const http = require("http")
 const socketio = require("socket.io")
+const Filter = require("bad-words")
+const {generateMessage, generateLocationMessage} = require("./utils/messages")
+const {
+    addUser,
+    getUser,
+    removeUser,
+    getUsersInRoom
+} = require("./utils/users")
 
 const port = process.env.PORT
 const app = express()
@@ -14,16 +22,55 @@ app.use(express.static(publicDirectoryPath))
 
 io.on("connection", (socket) => {
     console.log("New WebSocket connection")
-    
-    socket.emit("message", "Welcome")
-    
-    socket.broadcast.emit("message", "A new user has joined the chat.") //every client but this one receives the message
-    
-    socket.on("sendMessage", (msg) => {
-        io.emit("message", msg)
+     
+    socket.on("join", (options, callback) => {
+        const {error, user} = addUser({id: socket.id, ...options})
+
+        if(error)
+            return callback(error)
+
+        socket.join(user.room)
+        socket.emit("message", generateMessage(`Room - ${user.room}`, "Welcome"))
+        socket.broadcast.to(user.room).emit("message", generateMessage(`Room - ${user.room}`, `${user.username} has joined the room.`)) //every client in the room but this one receives the message
+        io.to(user.room).emit("roomData", {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+        callback()
     })
+
+    socket.on("sendMessage", (msg, callback) => {
+        const user = getUser(socket.id)
+        if(!user)
+            return callback("User doesn't exit.")
+        
+            const filter = new Filter()
+
+        if(filter.isProfane(msg))
+            msg = filter.clean(msg)
+
+        io.to(user.room).emit("message", generateMessage(user.username, msg))
+        callback()
+    })
+
+    socket.on("sendLocation", ({latitude, longitude}, callback) => {
+        const user = getUser(socket.id)
+        if(!user)
+            return callback("User doesn't exit.")
+
+        io.to(user.room).emit("locationMessage", generateLocationMessage(user.username, latitude, longitude))
+        callback()
+    })
+    
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left the chat.")
+        const user = removeUser(socket.id)
+        if(user) {
+            io.to(user.room).emit("message", generateMessage(`Room - ${user.room}`, `${user.username} has left the room.`))
+            io.to(user.room).emit("roomData", {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
     })
 })
 
