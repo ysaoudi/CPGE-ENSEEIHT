48c7dfabb390af074c15ed5896ef7c9c40710287
ysaoudi <younes.saoudi@outlook.fr>
2021-03-04 15:29:13 +0100

    removed project

--- ENSEEIHT/2ndYear/S8/WebDev/devnet-app/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.send("API RUNNING"))
-
-app.listen(PORT, () => console.log(`Server UP on port: ${PORT}`))
